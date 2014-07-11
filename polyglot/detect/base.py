9e6f5d539eb93794b5a3708e88627c7cc4a72524
Rami Al-Rfou <rmyeid@gmail.com>
2014-07-11 18:15:27 -0400

    enable automatic detection of language using peek, modernize the detector interface.

--- polyglot/detect/base.py
+++ polyglot/detect/base.py
@@ -4,14 +4,25 @@
 
 """Detecting languages"""
 
-
+from icu import Locale
 import pycld2 as cld2
 
 class Detector(object):
   def __init__(self):
     pass
 
+  @property
+  def name(self):
+    return self.locale.getDisplayLanguage()
+
+  @property
+  def code(self):
+    return self.locale.getName()
+
   def detect(self, text):
     flag, index, top_3_choices = cld2.detect(text)
     top_choice, second_choice, third_choice = top_3_choices
-    return top_choice[1:3]
+    basic_name, code, confidence, bytesize = top_choice
+    self.locale = Locale(code)
+    self.confidence = float(confidence)
+    return self
