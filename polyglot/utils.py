2495720b2928ccefa2a9a8cefbd034cb6279c946
Rami Al-Rfou <rmyeid@einstein.nbl.cewit.stonybrook.edu>
2014-08-09 18:24:11 -0400

    Move loading methods to a new module.

--- polyglot/utils.py
+++ polyglot/utils.py
@@ -5,43 +5,18 @@
 
 from os import path
 import os
-from . import data_path
-from mapping import Embedding
 
-if "~" in data_path:
-  data_path = path.expanduser(data_path)
-
-polyglot_path = path.join(path.abspath(data_path), "polyglot_data")
+from six import text_type as unicode
+from six import string_types
 
 
-resource_dir = {
-  "cw_embeddings":"embeddings2",
-  "visualization": "tsne2",
-}
+def _open(file_, mode='r'):
 
-def locate_resource(name, lang, filter=None):
-  """Return filename that contains specific language resource name.
-
-  Args:
-    name (string): Name of the resource.
-    lang (string): language code to be loaded.
-  """
-  task_dir = resource_dir[name]
-  p = path.join(polyglot_path, task_dir, lang)
-  if not path.isdir(p):
-    raise ValueError("This resource is not available "
-      "try to run\n\n$polyglot download {}.{}".format(name, lang))
-
-  return path.join(p, os.listdir(p)[0])
-
-
-def load_embeddings(lang="en", type="cw"):
-  """Return a word embeddings object for `lang` and of type `type`
-
-  Args:
-    lang (string): language code.
-    type (string): skipgram, cw, cbow ...
-  """
-  src_dir = "{}_embeddings".format(type)
-  p = locate_resource(src_dir, lang)
-  return Embedding.load(p) 
+  if isinstance(file_, string_types):
+    _, ext = path.splitext(file_)
+    if ext in {'.bz2', '.gz'}:
+      s = tarfile.open(file_)
+      return s.extractfile(s.next())
+    else:
+      return open(file_, mode)
+  return file_
