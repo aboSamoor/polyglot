d7dd295b913a1faf6bff879cda60635f34cc796d
Rami Al-Rfou <rmyeid@gmail.com>
2014-08-11 18:35:31 -0400

    Add pickling methods and register them. Static methods are still a problem.

--- polyglot/__init__.py
+++ polyglot/__init__.py
@@ -4,8 +4,14 @@ __author__ = 'Rami Al-Rfou'
 __email__ = 'rmyeid@gmail.com'
 __version__ = '14.11'
 
+import copy_reg
+import types
+
 from .base import Sequence, TokenSequence
+from .utils import _pickle_method, _unpickle_method
 
 __all__ = ['Sequence', 'TokenSequence']
 
 data_path = '~/'
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
--- polyglot/utils.py
+++ polyglot/utils.py
@@ -33,3 +33,33 @@ def _print(text):
     print(text)
   else:
     print(text.encode("utf8"))
+
+def _pickle_method(method):
+  """Pickle methods properly, including class methods."""
+  func_name = method.im_func.__name__
+  obj = method.im_self
+  cls = method.im_class
+  if isinstance(cls, type):
+      # handle classmethods differently
+      cls = obj
+      obj = None
+  if func_name.startswith('__') and not func_name.endswith('__'):
+      #deal with mangled names
+      cls_name = cls.__name__.lstrip('_')
+      func_name = '_%s%s' % (cls_name, func_name)
+
+  return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+  """Unpickle methods properly, including class methods."""
+
+  if obj is None:
+    return cls.__dict__[func_name].__get__(obj, cls)
+  for cls in cls.__mro__:
+    try:
+      func = cls.__dict__[func_name]
+    except KeyError:
+      pass
+    else:
+      break
+  return func.__get__(obj, cls)
