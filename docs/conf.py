b5e88fb0e8d407090be67dd283183e1f3b900c40
Rami Al-Rfou <rmyeid@gmail.com>
2014-09-07 17:35:41 -0400

    Another try to fix github module inclusion

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,13 @@
 import sys
 import os
 
+# If extensions (or modules to document with autodoc) are in another
+# directory, add these directories to sys.path here. If the directory
+# is relative to the documentation root, use os.path.abspath to make it
+# absolute, like shown here.
+sys.path.insert(0, os.path.abspath('sphinxext'))
 
-
-from .github_link import make_linkcode_resolve
+from github_link import make_linkcode_resolve
 
 # Try to override the matplotlib configuration as early as possible
 try:
