9d143ed1f0563c341e9d44f295ad0fcab3f1a2a7
Rami Al-Rfou <rmyeid@gmail.com>
2015-03-14 19:52:16 -0400

    Enable rtd theme locally

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@
 
 import sys
 import os
-#import sphinx_bootstrap_theme
-#import klink
-#import alabaster
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # Solution to RTD problem suggested by
 # http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
@@ -155,16 +155,25 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'default'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+  import sphinx_rtd_theme
+  html_theme = 'sphinx_rtd_theme'
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+  html_theme = 'default'
 
-#html_theme = 'klink'
-#html_theme_path = [klink.get_html_theme_path()]
-
+#import sphinx_bootstrap_theme
 #html_theme = 'bootstrap'
 #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
+#import klink
+#html_theme = 'klink'
+#html_theme_path = [klink.get_html_theme_path()]
+
+#import alabaster
 #html_theme = 'alabaster'
 #html_theme_path = [alabaster.get_path()]
+
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
