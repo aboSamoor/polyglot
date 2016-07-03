d28d8cf00e469f21ec03f259c8b2a2ccf9067bdb
Yingtao Tian <alan.yt.tian@gmail.com>
2016-07-03 13:02:18 -0700

    Fix typo.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ test_requirements = [
 setup(
     name='polyglot',
     version='16.07.04',
+    description='Polyglot is a natural language pipeline that supports massive multilingual applications.',
     long_description=readme + '\n\n' + history,
     author='Rami Al-Rfou',
     author_email='rmyeid@gmail.com',
