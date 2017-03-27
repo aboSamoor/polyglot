7f3de498ea9a90b058b70509036cf4b0ada0eb4d
Florian Delemarre <florian.delemarre@epitech.eu>
2017-03-27 11:10:32 +0200

    Remove readme read

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    # readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
