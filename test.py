bc67349058182ad4f537dd40b6618563e1da3c92
MSaudTahir <mohammadsaudtahir@gmail.com>
2023-02-24 23:32:19 +0500

    Update test.py

--- test.py
+++ test.py
@@ -13,3 +13,8 @@ from polyglot.text import Text
 def test_Text_words():
     text = Text("Hello and hi")
     assert(list(text.words) == ['Hello', 'and', 'hi'])
+
+
+def test_Text_sentences():
+    text = Text("Hello and hi. What and why.")
+    assert(list(text.sentences[1].words) == ['What', 'and', 'why'])
